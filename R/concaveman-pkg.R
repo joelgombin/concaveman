b68ce2e6216e75f4cb01262a412f17c3947b52a6
Joel Gombin <joel.gombin@gmail.com>
2017-07-25 12:10:32 +0200

    "R CMD check succeeded" \o/

---
+++ R/concaveman-pkg.R
@@ -0,0 +1,12 @@
+#' concaveman: A very fast 2D concave hull algorithm.
+#'
+#' This package is a simple R port (through [`V8`](https://github.com/jeroen/v8)) of a [JavaScript library by Vladimir Agafonkin](https://github.com/mapbox/concaveman).
+#'
+#'
+#' @docType package
+#' @name concaveman
+NULL
+
+## quiets concerns of R CMD check re: the .'s that appear in pipelines
+if(getRversion() >= "2.15.1")  utils::globalVariables(c("."))
+
