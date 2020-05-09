ab5ef7c680461e8f3e0ac7c0e7ef0e0e15d12e73
Joel Gombin <joel.gombin@gmail.com>
2020-05-10 00:28:52 +0200

    better and more modern code for the `points` data. The exemple from README now works.

--- data-raw/fixtures.R
+++ data-raw/fixtures.R
@@ -1,20 +1,13 @@
 ## mixtures data
 
-library(rjson)
-library(sp)
+library(jsonlite)
 library(sf)
-library(purrr)
-library(tidyverse)
 
-points <- fromJSON(file = "./inst/extdata/points-1k.json")
-
-points <- map_df(points, ~ data_frame(x = .[1], y = .[2]))
+points <- fromJSON("./inst/extdata/points-1k.json")
 
 means <- kmeans(points, centers = 10)
-points <- cbind(points, k = means$cluster)
 
-coordinates(points) <- c("x", "y")
-proj4string(points) <- "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"
+points <- tibble::tibble(x = points[,1], y = points[,2], k = means$cluster)
 
-points <- st_as_sf(points, crs = "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0")
+points <- st_as_sf(points, coords = c("x", "y"), crs = "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0")
 
