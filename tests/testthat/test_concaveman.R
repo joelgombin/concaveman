0ae03b49ce545b69dfb5b066fb6c3e8dec291d84
josiahparry <josiah.parry@gmail.com>
2022-06-17 14:32:43 -0400

    add test for output equality

--- tests/testthat/test_concaveman.R
+++ tests/testthat/test_concaveman.R
@@ -11,3 +11,12 @@ test_that("input and output formats works correctly", {
   expect_error(concaveman(array()))
 })
 
+test_that("outputs from methods are identical", {
+  cc_m <- unname(concaveman(sf::st_coordinates(points)))
+  cc_sf <- unname(sf::st_coordinates(concaveman(points))[,1:2])
+  cc_sfc <- unname(sf::st_coordinates(concaveman(sf::st_geometry(points)))[,1:2])
+  
+  expect_identical(cc_sf, cc_m)
+  expect_identical(cc_sfc, cc_m)
+  expect_identical(cc_sf, cc_sfc)
+})
