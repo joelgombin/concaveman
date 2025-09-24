bcf4ade6524d0b5f020e06af767dce8f87a480f3
Joel Gombin <joel.gombin@gmail.com>
2025-09-24 08:02:27 +0200

    add test for compatibility with tidyverse implementation

--- tests/testthat/test_concaveman.R
+++ tests/testthat/test_concaveman.R
@@ -15,8 +15,14 @@ test_that("outputs from methods are identical", {
   cc_m <- unname(concaveman(sf::st_coordinates(points)))
   cc_sf <- unname(sf::st_coordinates(concaveman(points))[,1:2])
   cc_sfc <- unname(sf::st_coordinates(concaveman(sf::st_geometry(points)))[,1:2])
-  
+
   expect_identical(cc_sf, cc_m)
   expect_identical(cc_sfc, cc_m)
   expect_identical(cc_sf, cc_sfc)
+})
+
+test_that("outputs from sf method is identical to tidyverse implemntation", {
+  # for compatibility with https://github.com/xzhoulab/SRTsim/blob/30dac3577674e250abc2f1de26dbed6a0bde64cb/R/simSRTLocs.R#L108 for example
+  ptns <- concaveman(points)
+  expect_no_error(as.data.frame(as.matrix(ptns$polygons[[1]])))
 })
